d49df8d55e7d5954874f058337f0b0e5bc1de0d8
Maxime <maxime.bettinelli@gmail.com>
2018-08-08 16:48:04 +0000

    Settings après django_heroku + Procfile

--- projetun/settings.py
+++ projetun/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -126,6 +126,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Pour Heroku
+django_heroku.settings(locals())
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
